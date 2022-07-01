78bc178879918fcc68665fa9c898d20f9c44d7cf
Putrya Evgeniy <user135@bulat.ru>
2022-07-01 14:26:37 +0300

    add AlgorithmsDemo

---
+++ src/main/java/com/Putrya_E/javacore/chapter18/AlgorithmsDemo.java
@@ -0,0 +1,42 @@
+package main.java.com.Putrya_E.javacore.chapter18;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class AlgorithmsDemo {
+    public static void main(String[] args) {
+        // создать неинициализированный связный список
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(-8);
+        ll.add(20);
+        ll.add(-20);
+        ll.add(8);
+
+        // создать компаратор с обратным упорядочением
+        Comparator<Integer> r = Collections.reverseOrder();
+
+        // отсортировать список с помощью этого компаратора
+        Collections.sort(ll, r);
+
+        System.out.println("Список отсортирован в обратном порядке: ");
+
+        for (int i : ll)
+            System.out.print(i + " ");
+
+        System.out.println();
+
+        // перетасовать список
+        Collections.shuffle(ll);
+
+        // вывести перетасованный список
+        System.out.println("Список перетасован: ");
+
+        for (int i : ll)
+            System.out.print(i + " ");
+
+        System.out.println();
+        System.out.println("Минимум: " + Collections.min(ll));
+        System.out.println("Максимум: " + Collections.max(ll));
+
+    }
+}
