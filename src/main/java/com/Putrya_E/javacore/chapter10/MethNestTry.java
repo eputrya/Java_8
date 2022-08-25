0c1434dc36ab332fbd3a86c88374472f80abe01d
eugene <garden8b103@gmail.com>
2022-08-25 16:33:58 +0300

    add MetNestTry

---
+++ src/main/java/com/Putrya_E/javacore/chapter10/MethNestTry.java
@@ -0,0 +1,39 @@
+package main.java.com.Putrya_E.javacore.chapter10;
+
+// Операторы try могут быть неявно вложены в вызовы методов.
+public class MethNestTry {
+    static void nesttry(int a) {
+        try { //вложенный блок try
+            /* Если укзазан один аргумент командной строки,
+               то исключение в связи с делением на нуль
+               будет сгенерировано в следующем коде. */
+            if (a == 1) a = a/(a-a); // деление на нуль
+
+            /* Если указаны два аргумента командной строки,
+               то генерируется исключение в связи с выходом
+               за пределы массива. */
+            if (a == 2) {
+                int[] c = { 1 };
+                c[42] = 99; // здесь генерируется исключение в связи
+                            // c выходом за пределы массива.
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Индекс за пределами массива: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            int a = args.length;
+
+            /* Если не указаны аргументы командной стоки,
+               в следующем операторе будет сгенерировано
+               исключение в связи с делением на нуль. */
+            int b = 42 / a;
+            System.out.println("a = " + a);
+            nesttry(a);
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на нуль: " + e);
+        }
+    }
+}
