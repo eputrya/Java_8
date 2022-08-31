f24f9ab03e96b730260068111312d647738fac27
eugene <garden8b103@gmail.com>
2022-08-31 12:39:46 +0300

    add DirListOnly

--- src/main/java/com/Putrya_E/javacore/chapter20/DirListOnly.java
+++ src/main/java/com/Putrya_E/javacore/chapter20/DirListOnly.java
@@ -1,2 +1,31 @@
-package main.java.com.Putrya_E.javacore.chapter20;public class DirListOnly {
+package main.java.com.Putrya_E.javacore.chapter20;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+class OnlyExt implements FilenameFilter {
+    String ext;
+
+    public OnlyExt(String ext) {
+        this.ext = ext;
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.endsWith(ext);
+    }
+}
+
+// Посмотреть каталог HTML-файлов
+public class DirListOnly {
+    public static void main(String[] args) {
+        String dirname = "/java";
+        File f1 = new File(dirname);
+        FilenameFilter only = new OnlyExt("html");
+        String[] s = f1.list(only);
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
 }
