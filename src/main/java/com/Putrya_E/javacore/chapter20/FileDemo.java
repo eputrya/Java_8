7be483b32cad4d37cd513bb5bff1cbffee77eeb4
eugene <garden8b103@gmail.com>
2022-08-31 12:40:12 +0300

    add FileDemo

--- src/main/java/com/Putrya_E/javacore/chapter20/FileDemo.java
+++ src/main/java/com/Putrya_E/javacore/chapter20/FileDemo.java
@@ -1,2 +1,26 @@
-package main.java.com.Putrya_E.javacore.chapter20;public class FileDemo {
+package main.java.com.Putrya_E.javacore.chapter20;
+
+import java.io.File;
+
+//Продемонстрировать применение некоторых методов из класса File
+public class FileDemo {
+    static void p(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        File f1 = new File("/java/COPYRIGHT");
+        p("Имя файла: " + f1.getName());
+        p("Путь: " + f1.getPath());
+        p("Абсолютный путь: " + f1.getAbsolutePath());
+        p("Родительский каталог: " + f1.getParent());
+        p(f1.exists() ? "существует" : "не существует");
+        p(f1.canWrite() ? "доступен для записи" : "не доступен для записи");
+        p(f1.canRead() ? "доступен для чтения" : "не доступен для чтения");
+        p(f1.isDirectory() ? "является каталогом" : "не является каталогом");
+        p(f1.isFile() ? "является обычным файлом" : "может быть именованным каналом");
+        p(f1.isAbsolute() ? "является абсолютным" : "не является абсолютным");
+        p("Последнее изменение в файле: " + f1.lastModified());
+        p("Размер: " + f1.length() + " байт");
+    }
 }
