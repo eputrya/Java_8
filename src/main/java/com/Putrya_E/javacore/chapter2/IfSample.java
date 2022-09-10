dc9e3a8f1dfe33c0bd62ff0e2b5474bc3c43ee57
eugene <garden8b103@gmail.com>
2022-09-10 22:00:37 +0300

    add IfSample

---
+++ src/main/java/com/Putrya_E/javacore/chapter2/IfSample.java
@@ -0,0 +1,25 @@
+package main.java.com.Putrya_E.javacore.chapter2;
+
+/*
+Продемонстрировать применение условного оператора if.
+Присвоить исходному файлу имя "IfSample.java"
+ */
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x меньше y");
+
+        x = x * 2;
+        if (x == y) System.out.println("x теперь равен y");
+
+        x = x * 2;
+        if (x > y) System.out.println("x теперь больше y");
+
+        // этот оператор не будет ничего выводить
+        if (x == y) System.out.println("вы не увидите этого");
+    }
+}
