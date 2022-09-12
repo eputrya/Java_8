4816f433551dedc3c4a9cd74f747567478b58498
eugene <garden8b103@gmail.com>
2022-09-12 09:31:22 +0300

    add Area

---
+++ src/main/java/com/Putrya_E/javacore/chapter3/Area.java
@@ -0,0 +1,14 @@
+package main.java.com.Putrya_E.javacore.chapter3;
+
+// Вычислить площадь круга
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8; // радиус окружности
+        pi = 3.1416; // приблизительное значение числа пи
+        a = pi * r * r; // вычислить площадь круга
+
+        System.out.println("Площадь круга раавна " + a);
+    }
+}
