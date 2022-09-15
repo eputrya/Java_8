3721255a32aa7a0a6eccaf32cd8aa5dbb48dbb6c
eugene <garden8b103@gmail.com>
2022-09-15 10:36:23 +0300

    add StringSwitch

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/StringSwitch.java
@@ -0,0 +1,22 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+// Использовать символьные строки для управления оператором switch
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "два";
+        switch (str) {
+            case "один":
+                System.out.println("один");
+                break;
+            case "два":
+                System.out.println("два");
+                break;
+            case "три":
+                System.out.println("три");
+                break;
+            default:
+                System.out.println("не совпало");
+                break;
+        }
+    }
+}
