fe7c47b9a124392f915f93dabe0daba7c8abd376
eugene <garden8b103@gmail.com>
2022-09-15 09:39:36 +0300

    add Ternary

---
+++ src/main/java/com/Putrya_E/javacore/chapter4/Ternary.java
@@ -0,0 +1,21 @@
+package main.java.com.Putrya_E.javacore.chapter4;
+
+// Продемонстрировать применение тернарной операции ?
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i; // получить абсолютное значения переменной i
+        System.out.print("Абсолютное значение  ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i; // получить абсолютное значения переменной i
+        System.out.println("Абсолютное значение  ");
+
+        System.out.println("Абсолютное значение  ");
+        System.out.println(i + " равно " + k);
+
+    }
+}
