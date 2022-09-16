6bd5b20d78c54b9c34338ba1bc1f1d6691e66e81
eugene <garden8b103@gmail.com>
2022-09-16 10:13:42 +0300

    add ForTick

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/ForTick.java
@@ -0,0 +1,11 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+// Продемонстрировать применение оператора цикла for
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--)
+            System.out.println("такт " + n);
+    }
+}
