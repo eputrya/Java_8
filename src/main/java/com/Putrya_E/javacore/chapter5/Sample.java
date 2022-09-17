a17e0e0f809405928306dc7c95f4b6a40168107d
eugene <garden8b103@gmail.com>
2022-09-17 15:38:26 +0300

    add SAmple

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/Sample.java
@@ -0,0 +1,14 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+public class Sample {
+    public static void main(String[] args) {
+        int a, b;
+
+        b = 4;
+        for (a = 0; a < b; a++) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            b--;
+        }
+    }
+}
