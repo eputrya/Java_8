ee58c5cf90db1402cc1c1e28a3edd77719f00fdd
eugene <garden8b103@gmail.com>
2022-09-17 15:51:40 +0300

    add ForEach2

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/ForEach2.java
@@ -0,0 +1,18 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+// Применение оператора break в цикле for в стиле for each
+public class ForEach2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // использовать цикл for в стиле for each для
+        // вывода и суммирования значений из части массива
+        for (int x : nums) {
+            System.out.println("Значение равно: " + x);
+            sum += x;
+            if (x == 5) break; // прервать цикл после получения 5 значений
+        }
+        System.out.println("Сумма пяти первых элементов равна: " + sum);
+    }
+}
