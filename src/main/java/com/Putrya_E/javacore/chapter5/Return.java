c3f0183059ab027c34cb5190144cfa4571527174
eugene <garden8b103@gmail.com>
2022-09-19 09:53:51 +0300

    add Return

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/Return.java
@@ -0,0 +1,14 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+// Продемонстрировать применение оператора return
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        System.out.println("До возврата.");
+
+        if (t) return; // возврат в вызывающий код
+
+        System.out.println("Этот оператор выполняться не будет.");
+    }
+}
