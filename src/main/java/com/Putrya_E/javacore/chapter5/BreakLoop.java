45acceca42d24c814b167cd2f520379427d72715
eugene <garden8b103@gmail.com>
2022-09-19 09:17:11 +0300

    add BreakLoop

---
+++ src/main/java/com/Putrya_E/javacore/chapter5/BreakLoop.java
@@ -0,0 +1,12 @@
+package main.java.com.Putrya_E.javacore.chapter5;
+
+// Применение оператора break для выхода из цикла
+public class BreakLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i == 10) break; // выход из цикла, если значение переменной i равно 10
+            System.out.println("i: " + i);
+        }
+        System.out.println("Цикл завершен.");
+    }
+}
