a28ea81709964bbf299725f9d801bea1ba1d74cc
eugene <garden8b103@gmail.com>
2022-09-23 14:27:59 +0300

    add GenericMethodRefDemo

--- src/main/java/com/Putrya_E/javacore/chapter15/GenericMethodRefDemo.java
+++ src/main/java/com/Putrya_E/javacore/chapter15/GenericMethodRefDemo.java
@@ -42,7 +42,7 @@ public class GenericMethodRefDemo {
         count = myOp(MyArrayOps::<Integer>countMatching, vals, 4);
         System.out.println("Массив vals содержит " + count + " числа 4");
 
-        count = myOp(MyArrayOps::<String>countMatching, strs, "Two");
+        count = myOp(MyArrayOps::<String>countMatching, strs, "Два");
         System.out.println("Массив strs содержит " + count + " числа два");
 
     }
