98aa0a32f03baa35f90ff38cca257029aa573e79
eugene <garden8b103@gmail.com>
2022-09-24 19:04:49 +0300

    add UseMethodRef

---
+++ src/main/java/com/Putrya_E/javacore/chapter15/UseMethodRef.java
@@ -0,0 +1,42 @@
+package main.java.com.Putrya_E.javacore.chapter15;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// Использовать ссылку на метод, чтобы найти
+// максимальное значение в коллекции
+class MyClass {
+    private int val;
+
+    MyClass(int v) {
+        val = v;
+    }
+
+    int getVal() {
+        return val;
+    }
+}
+
+public class UseMethodRef {
+    // Метод compare(), совместимый с аналогичным методом,
+    // определенным в интерфейсе Comparator<T>
+    static int compareMC(MyClass a, MyClass b) {
+        return a.getVal() - b.getVal();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<MyClass> al = new ArrayList<MyClass>();
+
+        al.add(new MyClass(1));
+        al.add(new MyClass(4));
+        al.add(new MyClass(2));
+        al.add(new MyClass(9));
+        al.add(new MyClass(3));
+        al.add(new MyClass(7));
+
+        // найти максимальное значение, используя метод compareMC
+        MyClass maxValObj = Collections.max(al, UseMethodRef::compareMC);
+
+        System.out.println("Максимальное значение равно: " + maxValObj.getVal());
+    }
+}
