d78b7cc5889d0bde3ef6a8c4dee0e29629b78f21
eugene <garden8b103@gmail.com>
2022-10-08 22:07:59 +0300

    add StringDemo

---
+++ src/main/java/com/Putrya_E/javacore/chapter7/StringDemo.java
@@ -0,0 +1,14 @@
+package main.java.com.Putrya_E.javacore.chapter7;
+
+// Продемонстрировать применение символьных строк
+public class StringDemo {
+    public static void main(String[] args) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = strOb1 + " и " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+    }
+}
