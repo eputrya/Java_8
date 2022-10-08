1240c402aa1e63e2317d1a6fd3c094cee013763c
eugene <garden8b103@gmail.com>
2022-10-08 21:50:48 +0300

    add InnerClassDemo

---
+++ src/main/java/com/Putrya_E/javacore/chapter7/InnerClassDemo.java
@@ -0,0 +1,25 @@
+package main.java.com.Putrya_E.javacore.chapter7;
+
+// Продемонстрировать применение внутреннего класса
+class Outer {
+    int outer_x = 100;
+
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+
+    // это внутренний класс
+    class Inner {
+        void display() {
+            System.out.println("вывод: outer_x = " + outer_x);
+        }
+    }
+}
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.test();
+    }
+}
