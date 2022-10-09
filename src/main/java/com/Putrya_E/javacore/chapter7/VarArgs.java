e095f7f842d2a97a30ae864f08c7de000fdef088
eugene <garden8b103@gmail.com>
2022-10-09 19:27:55 +0300

    add VarArgs

---
+++ src/main/java/com/Putrya_E/javacore/chapter7/VarArgs.java
@@ -0,0 +1,24 @@
+package main.java.com.Putrya_E.javacore.chapter7;
+
+// Продемонстрировать применение аргументов переменной длины
+public class VarArgs {
+    // теперь метод vaTest() объявляется с аргументами переменной длины
+    static void vaTest(int... v) {
+        System.out.print("Количество аргументов: " + v.length +
+                " Содержимое: ");
+
+        for (int x : v)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Обратите внимание на возможные способы вызова
+        // метод vaTest() с аргументами переменной длины
+        vaTest(10);       // 1 аргумент
+        vaTest(1, 2, 3);  // 3 аргумента
+        vaTest();             // без аргументов
+    }
+}
