e4e71aec5bcbd740d7cb74106adf3f8dc12e5dc0
eugene <garden8b103@gmail.com>
2022-10-09 19:11:37 +0300

    add CommandLine

---
+++ src/main/java/com/Putrya_E/javacore/chapter7/CommandLine.java
@@ -0,0 +1,9 @@
+package main.java.com.Putrya_E.javacore.chapter7;
+
+// Вывести все аргументы командной строки
+public class CommandLine {
+    public static void main(String[] args) {
+        for(int i =0; i<args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
