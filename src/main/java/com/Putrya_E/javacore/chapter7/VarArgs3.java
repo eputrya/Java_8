abbbcddb18474f62ac7fc5dd7d3979afaa2cd999
eugene <garden8b103@gmail.com>
2022-10-10 16:04:05 +0300

    add VarArgs3

---
+++ src/main/java/com/Putrya_E/javacore/chapter7/VarArgs3.java
@@ -0,0 +1,41 @@
+package main.java.com.Putrya_E.javacore.chapter7;
+
+// Аргументы переменной длины и перегрузка
+public class VarArgs3 {
+
+    static void vaTest(int... v) {
+        System.out.print("vaTest(int ...): " + "Количество аргументов: " +
+                v.length + " Содержимое: ");
+
+        for (int x : v)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+
+    static void vaTest(boolean... v) {
+        System.out.print("vaTest(boolean ...) " + "Количество аргументов: " +
+                v.length + " Содержимое: ");
+
+        for (boolean x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(String msg, int... v) {
+        System.out.print("vaTest(String, int ...): " +
+                msg + v.length + " Содержимое: ");
+
+        for (int x : v)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest(1, 2, 3);
+        vaTest("Проверка: ", 10, 20);
+        vaTest(true, false, false);
+    }
+
+}
