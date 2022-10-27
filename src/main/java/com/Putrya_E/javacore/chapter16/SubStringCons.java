26ab38bc3905b43801ec09bff803ce1e8a1d4967
eugene <garden8b103@gmail.com>
2022-10-27 09:30:57 +0300

    add SubStringCons

---
+++ src/main/java/com/Putrya_E/javacore/chapter16/SubStringCons.java
@@ -0,0 +1,12 @@
+package main.java.com.Putrya_E.javacore.chapter16;
+
+// Создать символьную строку из подмножества массива символов
+public class SubStringCons {
+    public static void main(String[] args) {
+        byte[] ascii = {65, 66, 67, 68, 69, 70};
+        String s1 = new String(ascii);
+        System.out.println(s1);
+        String s2 = new String(ascii, 2, 3);
+        System.out.println(s2);
+    }
+}
