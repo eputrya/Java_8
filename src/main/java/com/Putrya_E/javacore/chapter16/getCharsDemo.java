d92db75eefd5d6e9603c474e64f26d3ca7084ca8
eugene <garden8b103@gmail.com>
2022-10-28 10:32:54 +0300

    add getCharsDemo

---
+++ src/main/java/com/Putrya_E/javacore/chapter16/getCharsDemo.java
@@ -0,0 +1,12 @@
+package main.java.com.Putrya_E.javacore.chapter16;
+
+public class getCharsDemo {
+    public static void main(String[] args) {
+        String s = "Это демонстрация метода getChars().";
+        int start = 4;
+        int end = 8;
+        char[] buf = new char[end - start];
+        s.getChars(start, end, buf, 0);
+        System.out.println(buf);
+    }
+}
