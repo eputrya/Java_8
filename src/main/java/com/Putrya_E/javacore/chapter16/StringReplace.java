ebbc218d9e2bde557cfdc514d90fc7bb805dbf30
eugene <garden8b103@gmail.com>
2022-10-31 08:54:37 +0300

    add StringReplace

---
+++ src/main/java/com/Putrya_E/javacore/chapter16/StringReplace.java
@@ -0,0 +1,22 @@
+package main.java.com.Putrya_E.javacore.chapter16;
+
+// Замена подстроки
+public class StringReplace {
+    public static void main(String[] args) {
+        String org = "This is a test. This is, too.";
+        String search = "is";
+        String sub = "was";
+        String result = "";
+        int i;
+        do { // заменить все совпадающие подстроки
+            System.out.println(org);
+            i = org.indexOf(search);
+            if (i != -1) {
+                result = org.substring(0, i);
+                result = result + sub;
+                result = result + org.substring(i + search.length());
+                org = result;
+            }
+        } while (i != -1);
+    }
+}
