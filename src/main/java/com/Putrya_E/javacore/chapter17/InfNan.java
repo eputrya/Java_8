c0f08cc29f503914459fc9a7c6cb5c582ecfdbfe
eugene <garden8b103@gmail.com>
2022-11-03 11:51:52 +0300

    add InfNan

---
+++ src/main/java/com/Putrya_E/javacore/chapter17/InfNan.java
@@ -0,0 +1,12 @@
+package main.java.com.Putrya_E.javacore.chapter17;
+
+// Продемонстрировать применение методов isInfinite() и isNan()
+public class InfNan {
+    public static void main(String[] args) {
+        Double d1 = new Double(1 / 0.);
+        Double d2 = new Double(0 / 0.);
+
+        System.out.println(d1 + ": " + d1.isInfinite() + ", " + d1.isNaN());
+        System.out.println(d2 + ": " + d2.isInfinite() + ", " + d2.isNaN());
+    }
+}
