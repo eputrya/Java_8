2c6da88aca0846cdb0585e4ccf1ec0a570d1f759
eugene <garden8b103@gmail.com>
2022-11-18 09:22:49 +0300

    added FormatDemo6

---
+++ src/main/java/com/Putrya_E/javacore/chapter19/FormatDemo6.java
@@ -0,0 +1,17 @@
+package main.java.com.Putrya_E.javacore.chapter19;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+// Использовать относительные индексы, чтобы упростить
+// создание пользовательских форматов даты и времени
+public class FormatDemo6 {
+    public static void main(String[] args) {
+        Formatter fmt = new Formatter();
+        Calendar cal = Calendar.getInstance();
+
+        fmt.format("Today is day %te of %<tB, %<tY", cal);
+        System.out.println(fmt);
+        fmt.close();
+    }
+}
