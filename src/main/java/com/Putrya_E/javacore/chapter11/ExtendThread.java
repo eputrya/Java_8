0bdb5c2b7a6286d40561bd59d708568548cc9212
eugene <garden8b103@gmail.com>
2022-12-06 18:05:49 +0300

    added ExtendThread

---
+++ src/main/java/com/Putrya_E/javacore/chapter11/ExtendThread.java
@@ -0,0 +1,39 @@
+package main.java.com.Putrya_E.javacore.chapter11;
+
+// Создать второй поток исполнения, расширив класс Thread
+class NewThread2 extends Thread {
+    NewThread2() {
+        // создать новый поток исполнения
+        super("Демонстрационный поток");
+        System.out.println("Дочерний поток: " + this);
+        start(); // запустить поток на исполнение
+    }
+
+    // Точка входа во второй поток исполнеия
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Дочерний поток: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Дочерний поток прерван.");
+        }
+        System.out.println("Дочерний поток завершен.");
+    }
+}
+
+public class ExtendThread {
+    public static void main(String[] args) {
+        new NewThread2(); // Создать новый поток исполнения
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Главный поток: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван.");
+        }
+        System.out.println("Главный поток завершен.");
+    }
+}
