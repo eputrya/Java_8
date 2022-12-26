3d94fdaaf881bb265ffc20e0b6e716a98b8bfbe9
eugene <garden8b103@gmail.com>
2022-12-26 10:50:34 +0300

    added UCDemo

---
+++ src/main/java/com/Putrya_E/javacore/chapter22/UCDemo.java
@@ -0,0 +1,54 @@
+package main.java.com.Putrya_E.javacore.chapter22;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+
+// Продемонстрировать применение класса URLConnection
+public class UCDemo {
+    public static void main(String[] args) throws Exception {
+        int c;
+        URL hp = new URL("http://www.internic.net");
+        URLConnection hpCon = hp.openConnection();
+
+        // получить дату
+        long d = hpCon.getDate();
+        if (d == 0)
+            System.out.println("Сведения о дате отсутствуют.");
+        else
+            System.out.println("Дата: " + new Date(d));
+
+        // получить тип содержимого
+        System.out.println("Тип содержимого: " + hpCon.getContentType());
+
+        // получить дату срока действия ресурса
+        d = hpCon.getExpiration();
+        if (d == 0)
+            System.out.println("Сведения о дате истекает: " + new Date(d));
+
+        // получить дату последней модификации
+        d = hpCon.getLastModified();
+        if (d == 0)
+            System.out.println("Сведения о дате последней модификации.");
+        else
+            System.out.println("Дата последней модификации: " + new Date(d));
+
+        // получить длину содержимого
+        long len = hpCon.getContentLengthLong();
+        if (len == -1)
+            System.out.println("Длина содержимого недоступна.");
+        else
+            System.out.println("Длина содержимого: " + len);
+        if (len != 0) {
+            System.out.println("=== Содержимое ===");
+            InputStream input = hpCon.getInputStream();
+            while (((c = input.read()) != -1)) {
+                System.out.print((char) c);
+            }
+            input.close();
+        } else {
+            System.out.println("Содержимое недоступно.");
+        }
+    }
+}
